9604487b9354d92ee27f033fe4f594a50b8e83ff
Shreyas Shivakumar <pauliteshreyas@gmail.com>
2021-03-12 22:32:02 +0530

    testing build

--- server/auth/src/index.ts
+++ server/auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up Auth server...');
+  console.log('Starting up Auth server....');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not defined!');
   }
